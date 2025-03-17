b386d34bc4e081a7346041d43f9f53e0f43a0851
PeuXout <psi.pedropeixoto@gmail.com>
2025-03-17 13:36:26 -0300

    Create innitials.py

---
+++ innitials.py
@@ -0,0 +1,8 @@
+print("PPPPPPP                L")
+print("P       P              L")
+print("P         P            L")
+print("P        P             L")
+print("PPPPPPP                L")
+print("P                      L")
+print("P                      L")
+print("P                      LLLLLLLLLLLL")
